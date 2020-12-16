555a5ef4bd536e35a5775656bad7fd2ef6f4b928
Mr. Wilson <bwilson@macon.k12.mo.us>
2020-12-16 08:01:08 -0600

    Add project files.

---
+++ ConsoleApp6/Car.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp6
+{
+    class Car
+    {
+        public string model;
+
+        public string make;
+
+        public int mPH;
+
+        public int mPHChange;
+
+        public int GetSpeed(int mPH, int mPHChange)
+        {
+
+            mPH = mPH + mPHChange;
+
+            return mPH;
+
+
+
+        }
+    }
+}
---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Console.WriteLine("Hello World!");
+
+            Car myCar = new Car();
+
+            myCar.make = " Ford ";
+
+            myCar.model = " Fiesta ";
+
+            //string ans = ("My Car is a" + myCar.make + " " + myCar.model);
+
+            //Console.WriteLine(ans);
+
+            myCar.mPH = 0;
+
+            myCar.mPHChange = 20;
+
+            
+            myCar.mPH = myCar.GetSpeed(myCar.mPH, myCar.mPHChange);
+
+            myCar.mPH = myCar.GetSpeed(myCar.mPH, myCar.mPHChange);
+
+            Console.WriteLine(myCar.mPH);
+
+
+        }
+    }
+}
